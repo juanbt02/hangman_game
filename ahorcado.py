148dce99ab2c2afe2ae79da4d9c1eba03fdb34f3
juanbt02 <60817776+juanbt02@users.noreply.github.com>
2022-02-03 15:18:58 +0100

    Add files via upload

---
+++ ahorcado.py
@@ -0,0 +1,129 @@
+#Reglas: incoporar comprehensions, manejar archivo data.txt
+# Investigar funcion enumerate, el método get, os.system("clear")
+
+import random
+import os
+from xml.etree.ElementTree import tostring
+
+def getWord():
+    
+    i = 0
+    word = str
+    num_random = int(random.uniform(1,170))
+    final_word = []
+
+    with open("./archivos/data.txt", "r", encoding="utf-8") as f:
+        words = [line for line in f]
+        word = list(words[num_random])
+    f.close()
+
+    while i < len(word) -1:
+        final_word.append(word[i])
+        i+=1
+
+    return final_word
+        
+
+def run():
+    
+
+    handed_picture = [
+        """--------------||
+                 |
+           ||             
+           ||   
+           ||   
+           ||___________
+        """,
+        """--------------||
+                 |
+           ||    O         
+           ||   
+           ||   
+           ||___________
+        """,
+        """--------------||
+                 |
+           ||    O         
+           ||   {
+           ||   
+           ||___________
+        """,
+        """--------------||
+                 |
+           ||    O         
+           ||   {|
+           ||   
+           ||___________
+        """,
+        """--------------||
+                 |
+           ||    O         
+           ||   {|}
+           ||   
+           ||___________
+        """,
+        """--------------||
+                 |
+           ||    O         
+           ||   {|}
+                /
+           ||   
+           ||___________
+        """,
+        """--------------||
+
+           ||             
+           ||     O
+           ||    /|\ 
+           ||___ / \  ___
+                iiiii
+        """,
+    ]
+
+    intentos = 6
+    fallos = 0
+    word = getWord()
+    underscore_word = []
+    for i in word:
+        underscore_word.append("_")
+
+
+    while True:
+        os.system("clear")
+        print("!ADIVINA LA PALABRA!\n")
+        print(handed_picture[fallos])
+        print(str(underscore_word), "\n")
+        index = 0
+        confirmador = 0
+        n = input("Introduce una letra: ")
+        if fallos < intentos:
+            while index < len(word):
+                if word[index] == n:
+                    underscore_word[index] = n
+                    confirmador+=1 
+                index+=1
+            if confirmador == 0:
+                fallos+=1
+            
+        if fallos == intentos:
+            os.system("clear")
+            print(handed_picture[6])
+            print("Has sido ahorcado, la palabra era: ", "".join(word))
+            break
+
+        if underscore_word == word:
+            os.system("clear")
+            print("!ENHORABUENA, LO HAS ADIVINADO¡")
+            break
+    
+
+        
+            
+    
+
+    
+
+
+if __name__ == '__main__':
+    run()
